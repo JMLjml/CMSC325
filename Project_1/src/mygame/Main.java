9d06577279c2aaa6dada6edcd0ee80e67e604726
JMLjml <johnlasheski@gmail.com>
2015-02-03 21:07:51 -0500

    Project 1 is complete

--- Project_1/src/mygame/Main.java
+++ Project_1/src/mygame/Main.java
@@ -1,15 +1,40 @@
 package mygame;
 
+
+import appstate.InputAppState;
+import physics.PhysicsTestHelper;
+import characters.MyGameCharacterControl;
+
+
 import com.jme3.app.SimpleApplication;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Spatial;
-
+import com.jme3.bullet.BulletAppState;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.scene.Geometry;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * test
  * @author John M Lasheski
  */
 public class Main extends SimpleApplication {
+    
+    protected BulletAppState bulletAppState;
+    private RigidBodyControl landscape;
+    
+    private List<Geometry> targets = new ArrayList<Geometry>();
+    
+      
+    private Vector3f normalGravity = new Vector3f(0, -9.81f, 0);
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -18,15 +43,116 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        // Set the start position for he camera
+        cam.getLocation().set(-10,12,25);
+        cam.update();
         
+        // Add the Scene And create collision physics to match the terrain
         Spatial scene = assetManager.loadModel("/Scenes/HW_2_Scene.j3o");
+                
+        CollisionShape sceneShape = 
+                CollisionShapeFactory.createMeshShape(scene);
+        landscape = new RigidBodyControl(sceneShape, 0);
+        scene.addControl(landscape);
+        
+        //Attach the scene to the root
         rootNode.attachChild(scene);
+        
+        // Allows for the use of Physics simulation
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        
+        // Add the scene to the PhysicsSpace
+        bulletAppState.getPhysicsSpace().add(landscape);
+        
+        //Create the Physics World based on the Helper class
+        PhysicsTestHelper.createMyPhysicsTestWorld(rootNode, assetManager, 
+                bulletAppState.getPhysicsSpace(), targets);
+        
+        // Add the Player to the world and use the customer character 
+        // and input control classes
+        Node sinbad = (Node)assetManager.loadModel("/Models/Sinbad/Sinbad.mesh.xml");
+        Node jaime = (Node)assetManager.loadModel("/Models/Jaime/Jaime.j3o");
+
+        // Set the starting postion and scale for the characters
+        sinbad.setLocalTranslation(0, 12, 0);
+        jaime.setLocalTranslation(-12, 6, 5);
+        jaime.setLocalScale(4, 4, 4);
+
+        
+        // Add the controls for the characters        
+        MyGameCharacterControl sinbadControl = new MyGameCharacterControl(0.5f,2.5f,8f);
+        MyGameCharacterControl jaimeControl = new MyGameCharacterControl(0.5f,2.5f,8f);
+        sinbadControl.setCamera(cam);
+        jaimeControl.setCamera(cam);
+        sinbad.addControl(sinbadControl);
+        jaime.addControl(jaimeControl);
+
+        // Set the physics for the characters
+        sinbadControl.setGravity(normalGravity);
+        jaimeControl.setGravity(normalGravity);
+        bulletAppState.getPhysicsSpace().add(sinbadControl);
+        bulletAppState.getPhysicsSpace().add(jaimeControl);
+        
+       
+        // Add the input listeners for the characters       
+        InputAppState sinbadAppState = new InputAppState();
+        sinbadAppState.setCharacter(sinbadControl);
+        stateManager.attach(sinbadAppState);
+        InputAppState jaimeAppState = new InputAppState();
+        jaimeAppState.setCharacter(jaimeControl);
+        stateManager.attach(jaimeAppState);
+        
+        // Attach the characters to the root node        
+        rootNode.attachChild(sinbad);
+        rootNode.attachChild(jaime);
+                
+         
+        //Add a custom font and text to the scene
+        BitmapFont myFont = assetManager.loadFont("Interface/Fonts/DroidSansMono.fnt");
+        
+
+        // Create the Text that will be used for displaying hits
+        BitmapText hitText = new BitmapText(myFont, true);
+        hitText.setName("hitText");
+        hitText.setText("HIT !!!");
+        hitText.setColor(ColorRGBA.Red);
+        hitText.setSize(guiFont.getCharSet().getRenderedSize());   
+        hitText.setLocalTranslation(settings.getWidth() / 2 - hitText.getLineWidth() / 2,
+            settings.getHeight() - 40, 0f);
+
+
+        // Create the crosshairs for the ball shooter
+        BitmapText crosshairs = new BitmapText(myFont, true); 
+        crosshairs.setText("X");
+        crosshairs.setColor(ColorRGBA.Yellow);
+        crosshairs.setSize(guiFont.getCharSet().getRenderedSize());
+        crosshairs.setLocalTranslation(settings.getWidth() / 2,
+            settings.getHeight() / 2 + crosshairs.getLineHeight(), 0f);
+        guiNode.attachChild(crosshairs);                 
+                
+        
+
+        // Create the title text for the hud
+        BitmapText hudText = new BitmapText(myFont, true);
+        hudText.setText("CMSC325 Project 1");
+        hudText.setColor(ColorRGBA.Blue);
+        hudText.setSize(guiFont.getCharSet().getRenderedSize());
+        hudText.setLocalTranslation(settings.getWidth() / 2 - hudText.getLineWidth() / 2 ,
+            settings.getHeight() - 10, 0f);
+        guiNode.attachChild(hudText);
+        
+
+        //Add the "bullets" to the scene to allow the player to shoot the balls
+        PhysicsTestHelper.createBallShooter(this,rootNode,bulletAppState.getPhysicsSpace(),
+            sinbadAppState, targets, guiNode, hitText);
     }
+
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
     }
+
 
     @Override
     public void simpleRender(RenderManager rm) {
---
+++ Project_1/src/physics/PhysicsTestHelper.java
@@ -0,0 +1,133 @@
+
+package physics;
+
+import appstate.InputAppState;
+import com.jme3.app.Application;
+import com.jme3.asset.AssetManager;
+import com.jme3.asset.TextureKey;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.MeshCollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.font.BitmapText;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Dome;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.scene.shape.Sphere.TextureMode;
+import com.jme3.texture.Texture;
+import java.util.List;
+
+/**
+ *
+ * @author normenhansen
+ * @author further modified by John M. Lasheski for CMSC325 Spring 2015
+ */
+public class PhysicsTestHelper {    
+
+    public static void createMyPhysicsTestWorld(Node rootNode, AssetManager assetManager,
+        PhysicsSpace space, List<Geometry> targets)  {        
+        
+        AmbientLight light = new AmbientLight();
+        light.setColor(ColorRGBA.LightGray);
+        rootNode.addLight(light);
+
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        material.setTexture("ColorMap", assetManager.loadTexture("Blender/2.4x/WoodCrate_lighter.png"));
+       
+
+
+        // create the movable boxes and assign them as targets
+        for (int i = 0; i < 25; i++) {
+            Box box = new Box(5, 5, 5);
+            Geometry boxGeometry = new Geometry("box", box);
+            boxGeometry.setMaterial(material);
+            boxGeometry.setLocalTranslation(-.005f * i, 12 * i,-40 );
+            //RigidBodyControl automatically uses sphere collision shapes when attached to single geometry with pqt mesh
+            boxGeometry.addControl(new RigidBodyControl(.001f));
+            boxGeometry.getControl(RigidBodyControl.class).setRestitution(1f);
+            rootNode.attachChild(boxGeometry);
+            space.add(boxGeometry);
+            
+            targets.add(boxGeometry);
+        }
+        
+        
+
+        // create immovable Pyramids with mesh collision shape, these are not targets
+        Material pyramidMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        pyramidMaterial.setTexture("ColorMap", assetManager.loadTexture("Textures/Terrain/splat/fortress512.png"));
+
+        for(int i = 0; i < 5; i++) {
+            Dome pyramid = new Dome(2,4, 2 * i + 3);
+            Geometry pyramidGeometry = new Geometry("pyramid", pyramid);
+
+            pyramidGeometry.setMaterial(pyramidMaterial);
+            pyramidGeometry.setLocalTranslation((15 * i) -25, 8, 2 * i -25);
+
+            float[] angles = {0, i * 12, 0};
+            pyramidGeometry.setLocalRotation(new Quaternion(angles));
+
+            pyramidGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(pyramid), 0));
+            rootNode.attachChild(pyramidGeometry);
+            space.add(pyramidGeometry);
+        }        
+    }
+
+
+
+    /**
+     * creates the necessary inputlistener and action to shoot balls from teh camera
+     * @param app
+     * @param rootNode
+     * @param space
+     */
+    public static void createBallShooter(final Application app, final Node rootNode,
+        final PhysicsSpace space, final InputAppState character, final List<Geometry> targets,
+        final Node guiNode, final BitmapText hitText) {
+        
+        ActionListener actionListener = new ActionListener() {
+
+            public void onAction(String name, boolean keyPressed, float tpf) {
+                Sphere bullet = new Sphere(22, 22, .3f, true, false);
+                bullet.setTextureMode(TextureMode.Projected);
+                Material mat2 = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+                TextureKey key2 = new TextureKey("Blender/2.4x/textures/Grass_256.png");
+                key2.setGenerateMips(true);
+                Texture tex2 = app.getAssetManager().loadTexture(key2);
+                mat2.setTexture("ColorMap", tex2);
+                if (name.equals("shoot") && !keyPressed) {
+                    Geometry bulletg = new Geometry("bullet", bullet);
+                    bulletg.setMaterial(mat2);
+                    bulletg.setShadowMode(ShadowMode.CastAndReceive);
+                    bulletg.setLocalTranslation(app.getCamera().getLocation());
+                    RigidBodyControl bulletControl = new RigidBodyControl(10);
+                    bulletg.addControl(bulletControl);
+                    bulletControl.setLinearVelocity(app.getCamera().getDirection().mult(150));
+                    bulletg.addControl(bulletControl);
+                    rootNode.attachChild(bulletg);
+                    space.add(bulletControl);
+                    
+                    
+                   
+                    // Delete the hit message now that we have shot again
+                    guiNode.detachChildNamed("hitText");
+                    
+                    // Have the character InputAppState check for hits
+                    character.fire(targets, guiNode, hitText);
+                }
+            }        
+            
+        };
+        app.getInputManager().addMapping("shoot", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+        app.getInputManager().addListener(actionListener, "shoot");
+    }
+}
